b427c1547fd07a4fc40d5df75938b408d5d15c4f
egeakman <egeakmanegeakman@hotmail.com>
2022-05-06 22:03:03 +0300

    Ready for pre-production

--- gui.py
+++ gui.py
@@ -18,9 +18,16 @@ deduplication_threshold = 0.9
 numOfKeywords = 1
 image = NULL
 reso_image = NULL
-custom_kw_extractor = yake.KeywordExtractor(lan=language, n=max_ngram_size, dedupLim=deduplication_threshold, top=numOfKeywords, features=None)
+custom_kw_extractor = yake.KeywordExtractor(
+    lan=language,
+    n=max_ngram_size,
+    dedupLim=deduplication_threshold,
+    top=numOfKeywords,
+    features=None,
+)
 
 from styles import styles
+
 
 class Window(QWidget):
     def __init__(self, parent=None):
@@ -36,7 +43,9 @@ class Window(QWidget):
         self.image.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
         self.label = QLabel("Preparing...", self)
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop)
+        self.label.setAlignment(
+            Qt.AlignmentFlag.AlignCenter | Qt.AlignmentFlag.AlignTop
+        )
         self.label.setStyleSheet(styles.text_style)
         self.label.setVisible(False)
 
@@ -47,7 +56,7 @@ class Window(QWidget):
         self.textInput.setStyleSheet(styles.text_edit_style)
         self.textInput.setPlaceholderText("Enter text here (280 character limit)")
 
-        self.resolution_list = ["Resolution","256", "512", "736"]
+        self.resolution_list = ["Resolution", "256", "512", "736"]
         self.numIterations_list = ["NumIterations", "400", "600", "800"]
 
         self.resolution_combobox = QComboBox()
@@ -94,8 +103,7 @@ class Window(QWidget):
         self.layout1.addWidget(self.label)
         self.layout1.addWidget(self.image)
         self.layout1.addStretch()
-        
-        
+
         self.layout.addStretch()
         self.layout.addLayout(self.layout1)
         self.layout.addStretch()
@@ -103,55 +111,91 @@ class Window(QWidget):
     def timeout(self):
         global keywords
         keywords = custom_kw_extractor.extract_keywords(self.textInput.toPlainText())
-        print(keywords)
-        if(keywords == []):
-            self.message_box(QMessageBox.Warning, QMessageBox.Ok, "Warning", "No keywords found. Please enter some text.")
+        # print(keywords)
+        if keywords == []:
+            self.message_box(
+                QMessageBox.Warning,
+                QMessageBox.Ok,
+                "Warning",
+                "No keywords found. Please enter some text.",
+            )
             return
-        if (self.textInput.toPlainText() == ""):
-            self.message_box(QMessageBox.Icon.Warning, QMessageBox.Ok, "Warning", "Please enter a valid text input.")
-        elif(len(self.textInput.toPlainText()) > 280):
-            self.message_box(QMessageBox.Icon.Warning, QMessageBox.Ok, "Warning", "There is a 280 character limit on the text input.")
+        if self.textInput.toPlainText() == "":
+            self.message_box(
+                QMessageBox.Icon.Warning,
+                QMessageBox.Ok,
+                "Warning",
+                "Please enter a valid text input.",
+            )
+        elif len(self.textInput.toPlainText()) > 280:
+            self.message_box(
+                QMessageBox.Icon.Warning,
+                QMessageBox.Ok,
+                "Warning",
+                "There is a 280 character limit on the text input.",
+            )
         elif self.resolution_combobox.currentText() == "Resolution":
-            self.message_box(QMessageBox.Icon.Warning, QMessageBox.Ok, "Warning", "Please select a valid resolution.")
-        elif (self.numIterations_combobox.currentText() == "NumIterations"):
-            self.message_box(QMessageBox.Icon.Warning, QMessageBox.Ok, "Warning", "Please select a valid number of iterations.")
+            self.message_box(
+                QMessageBox.Icon.Warning,
+                QMessageBox.Ok,
+                "Warning",
+                "Please select a valid resolution.",
+            )
+        elif self.numIterations_combobox.currentText() == "NumIterations":
+            self.message_box(
+                QMessageBox.Icon.Warning,
+                QMessageBox.Ok,
+                "Warning",
+                "Please select a valid number of iterations.",
+            )
         else:
-            self.label.setVisible(True)
-            self.message_box(QMessageBox.Icon.Information, QMessageBox.Ok, "Information", "Creating image process has been started.")
-            self.create_image()
-            image = cv2.imread("created_image.png")
-            if(self.resolution_combobox.currentText() == "256"):
-                reso_image = cv2.resize(image,(256,256))
-            if(self.resolution_combobox.currentText() == "512"):
-                reso_image = cv2.resize(image,(512,512))
-            if(self.resolution_combobox.currentText() == "736"):
-                reso_image = cv2.resize(image,(736,736))
-            # cv2.imwrite("created_image.png",reso_image)
-            
+            self.timeout_subfunc()
 
-        
+    def timeout_subfunc(self):
+        self.label.setVisible(True)
+        self.message_box(
+            QMessageBox.Icon.Information,
+            QMessageBox.Ok,
+            "Information",
+            "Creating image process has been started.",
+        )
+        self.create_image()
+
+        # image = cv2.imread("created_image.png")
+        # if self.resolution_combobox.currentText() == "256":
+        #     reso_image = cv2.resize(image, (256, 256))
+        # if self.resolution_combobox.currentText() == "512":
+        #     reso_image = cv2.resize(image, (512, 512))
+        # if self.resolution_combobox.currentText() == "736":
+        #     reso_image = cv2.resize(image, (736, 736))
+        #     # cv2.imwrite("created_image.png",reso_image)
+
     def create_image(self):
         word = keywords[0][0]
-        print(word)
+        # print(word)
         translated_word = translator.translate(word)
-        print(translated_word.text)
+        # print(translated_word.text)
         r = requests.post(
             "https://api.deepai.org/api/text2img",
             data={
-                'text': translated_word.text,
+                "text": translated_word.text,
             },
-            headers={'api-key': '0d0bb15f-c305-4d4c-b63d-cbb563e35a25'}
+            headers={"api-key": "0d0bb15f-c305-4d4c-b63d-cbb563e35a25"},
         )
 
-        print(r.json())
+        # print(r.json())
         response = requests.get(r.json()["output_url"])
-        file = open(f"{self.dir}/created_image.png", "wb")
-        file.write(response. content)
-        file.close()
-        pixmap = QtGui.QPixmap(f'{self.dir}/created_image.png')
+        with open(f"{self.dir}/created_image.png", "wb") as file:
+            file.write(response.content)
+        pixmap = QtGui.QPixmap(f"{self.dir}/created_image.png")
         self.image.setPixmap(pixmap)
         self.label.setVisible(False)
-        self.message_box(QMessageBox.Information, QMessageBox.Ok, "Success", f"Image created as {self.dir}/created_image.png.")
+        self.message_box(
+            QMessageBox.Information,
+            QMessageBox.Ok,
+            "Success",
+            f"Image created as {self.dir}/created_image.png.",
+        )
 
     def message_box(self, icon, buttons, title, text):
         msg = QMessageBox()
@@ -160,7 +204,6 @@ class Window(QWidget):
         msg.setStandardButtons(buttons)
         msg.setWindowTitle(title)
         return msg.exec()
-                    
 
     def modify_output_path(self):
         self.temp_path = self.dir
@@ -170,13 +213,12 @@ class Window(QWidget):
         if self.dir == "":
             self.dir = self.temp_path
 
+
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setStyleSheet("background-color: rgba(255, 255, 255, .1);")
-        self.setGeometry(
-            400, 200, 1200, 600
-        )
+        self.setGeometry(400, 200, 1200, 600)
         self.startMainMenu()
 
     def startMainMenu(self):
@@ -184,6 +226,7 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Artymind")
         self.setCentralWidget(self.window)
         self.showMaximized()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from rake_nltk import Rake
+
 rake_nltk_var = Rake()
+
 text = """
 Schmidt had had several years of psychotherapy and was not without some perspective on himself, and he knew that a certain percentage of his reaction to the way these older men coolly inspected their cuticles or pinched at the crease in the trouser of the topmost leg as they sat back on the coccyx joggling the foot of their crossed leg was just his insecurity, that he felt somewhat sullied and implicated by the whole enterprise of contemporary marketing and that this sometimes manifested via projection as the feeling that people he was trying to talk as candidly as possible to always believed he was making a sales pitch or trying to manipulate them in some way, as if merely being employed, however ephemerally, in the great grinding US marketing machine had somehow colored his whole being  and that something essentially shifty or pleading in his expression now always seemed inherently false or manipulative and turned people off, and not just in his career – which was not his whole existence, unlike so many at Team Δy, or even that terribly important to him; he had a vivid and complex inner life, and introspected a great deal – but in his personal affairs as well, and that somewhere along the line his professional marketing skills had metastasized through his whole character so that he was now the sort of man who, if he were to screw up his courage and ask a female colleague out for drinks and over drinks open his heart to her and reveal that he respected her enormously, that his feelings for her involved elements of both professional and highly personal regard, and that he spent a great deal more time thinking about her than she probably had any idea he did, and that if there were anything at all he could ever do to make
 """
 rake_nltk_var.extract_keywords_from_text(text)
 keyword_extracted = rake_nltk_var.get_ranked_phrases()
+
 print(keyword_extracted)
